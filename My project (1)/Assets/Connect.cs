de50d51c101b24f232ddaee2d7fa47250f635ced
hieu112h <hieu112h@gmail.com>
2024-04-24 21:55:37 +0700

    ok
    ok

---
+++ My project (1)/Assets/Connect.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+
+public class Connect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        My
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
